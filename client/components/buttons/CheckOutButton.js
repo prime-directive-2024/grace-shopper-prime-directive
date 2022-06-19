4c2b82e00835ab3bd31af7b947f181f436153c6f
Miguel Cruz <rednu7@yahoo.com>
2022-06-19 10:20:50 -0400

    renamed variables to English

--- client/components/buttons/CheckOutButton.js
+++ client/components/buttons/CheckOutButton.js
@@ -6,9 +6,30 @@ import { checkoutCart } from '../../store/cart';
 import { connect } from 'react-redux';
 //import function from store
 
+// const stripeCheckout = () => {
+//   fetch('/create-checkout-session', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify({
+//       items: [
+//         { id: 1, quantity: 2 },
+//         { id: 2, quantity: 3 },
+//       ],
+//     }),
+//   })
+//     .then((res) => {
+//       if (res.ok) return res.json();
+//       return res.json().then((json) => Promise.reject(json));
+//     })
+//     .then(({ url }) => (window.location = url));
+// };
+
 const CheckOutButton = (props) => {
   const dispatch = useDispatch();
   const handleClick = () => {
+    // stripeCheckout();
     dispatch(checkoutCart(props.userId, props.basket));
   };
   return (

c4c4222b653457bdeea744ec50c3cba79f4fbb2d
Havin Mickel <12345678900qw@gmail.com>
2022-06-20 07:04:40 -0700

    Added the ability to edit/delete albums, not secure yet

--- client/components/SingleAlbumView.js
+++ client/components/SingleAlbumView.js
@@ -1,42 +1,144 @@
-import React from 'react';
+/** @format */
+
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUser, updateUser } from '../store/singleUser';
 import { connect } from 'react-redux';
-import { getSingleAlbum } from '../store/singleAlbum';
+import {
+  deleteSingleAlbum,
+  getSingleAlbum,
+  updateSingleAlbum,
+} from '../store/singleAlbum';
 
-class SingleAlbumView extends React.Component {
-  componentDidMount() {
-    const id = this.props.match.params.id;
-    this.props.getSingleAlbum(id);
-  }
-
-  //
-  render() {
-    const album = this.props.album || {};
-    const songs = album.Songs || [];
-    console.log(album);
-    return (
-      <div>
-        <h1>
-          {album.title} ${album.price}
-        </h1>
-        <img src={album.img_url} />
-        <ul>
-          {songs.map((song) => (
-            <li key={song.id}>{song.title}</li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
-}
-
-const mapStateToProps = (state) => ({
-  album: state.singleAlbum,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  getSingleAlbum: (id) => {
+const SingleAlbumView = (props) => {
+  const album = useSelector((state) => state.singleAlbum || {});
+  const dispatch = useDispatch();
+  const [albumForm, setAlbumForm] = useState({
+    id: album.id,
+    title: album.title,
+    price: album.price,
+    img: album.img_url,
+    genre: album.genre,
+    editing: false,
+  });
+  useEffect(() => {
+    const id = props.match.params.id;
     dispatch(getSingleAlbum(id));
-  },
-});
+    setAlbumForm({
+      ...albumForm,
+      id: album.id,
+      title: album.title,
+      price: album.price,
+      img: album.img_url,
+      genre: album.genre,
+    });
+  }, [2]);
 
-export default connect(mapStateToProps, mapDispatchToProps)(SingleAlbumView);
+  const handleChange = (event) => {
+    setAlbumForm({
+      ...albumForm,
+      [event.target.name]: event.target.value,
+    });
+  };
+  const handleSubmit = () => {
+    dispatch(updateSingleAlbum(albumForm));
+    setAlbumForm({ ...albumForm, editing: !albumForm.editing });
+  };
+  const songs = album.Songs || [];
+
+  return (
+    <div>
+      <h1>
+        Title:
+        {albumForm.editing ? (
+          <input
+            type='text'
+            name='title'
+            value={albumForm.title}
+            onChange={handleChange}
+            placeholder={albumForm.title}
+          />
+        ) : (
+          album.title
+        )}
+      </h1>
+
+      <h2>
+        $
+        {albumForm.editing ? (
+          <input
+            type='text'
+            name='price'
+            value={albumForm.price}
+            onChange={handleChange}
+            placeholder={albumForm.price}
+          />
+        ) : (
+          album.price
+        )}
+      </h2>
+      <h2>
+        Genre:{' '}
+        {albumForm.editing ? (
+          <input
+            type='text'
+            name='genre'
+            value={albumForm.genre}
+            onChange={handleChange}
+            placeholder={albumForm.genre}
+          />
+        ) : (
+          album.genre
+        )}
+      </h2>
+      {albumForm.editing ? (
+        <>
+          <label>Enter image url here: </label>
+          <input
+            type='text'
+            name='img'
+            value={albumForm.img}
+            onChange={handleChange}
+            placeholder={albumForm.img}
+          />
+        </>
+      ) : (
+        <img src={album.img_url} />
+      )}
+      <div>
+        {songs.map((song) => (
+          <div key={song.id}>
+            <div key={song.id}>{song.title}</div>
+            {albumForm.editing ? <button>Delete song</button> : <></>}
+          </div>
+        ))}
+        {albumForm.editing ? <button>Add song</button> : <></>}
+      </div>
+      <button
+        onClick={() => dispatch(deleteSingleAlbum(props.match.params.id))}
+      >
+        Delete
+      </button>
+      {albumForm.editing ? (
+        <button onClick={() => handleSubmit()}>Save Changes</button>
+      ) : (
+        <button
+          onClick={() =>
+            setAlbumForm({
+              id: album.id,
+              title: album.title,
+              price: album.price,
+              img: album.img_url,
+              genre: album.genre,
+              editing: !albumForm.editing,
+            })
+          }
+        >
+          Edit Album
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default connect()(SingleAlbumView);
--- server/api/albums.js
+++ server/api/albums.js
@@ -1,10 +1,12 @@
-const router = require("express").Router();
+/** @format */
+const { requireToken, isAdmin } = require('./gateKeepingMiddleware');
+const router = require('express').Router();
 const {
   models: { Album, Artist, Song },
-} = require("../db");
+} = require('../db');
 module.exports = router;
 
-router.get("/:id", async (req, res, next) => {
+router.get('/:id', async (req, res, next) => {
   try {
     const album = await Album.findByPk(req.params.id, {
       include: [
@@ -23,15 +25,58 @@ router.get("/:id", async (req, res, next) => {
   }
 });
 
-router.get("/", async (req, res, next) => {
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const album = await Album.findByPk(req.params.id);
+    if (album) {
+      await Album.destroy({ where: { id: req.params.id } });
+      res.sendStatus(200);
+    } else {
+      new Error("Album doesn't exist!");
+      next(Error);
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    const { title, price, genre, img_url } = req.body;
+    const album = await Album.findByPk(req.params.id);
+    if (album) {
+      await album.update({ title, price, genre, img_url });
+      const newAlbum = await Album.findByPk(req.params.id, {
+        include: [
+          {
+            model: Artist,
+          },
+          {
+            model: Song,
+          },
+        ],
+      });
+      res.send(newAlbum);
+    } else {
+      new Error("Album doesn't exist");
+      next(Error);
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/', async (req, res, next) => {
   try {
     const albums = await Album.findAll({
-      include: [{
-        model: Artist,
-      },
-      {
-        model: Song,
-      }],
+      include: [
+        {
+          model: Artist,
+        },
+        {
+          model: Song,
+        },
+      ],
     });
     res.json(albums);
   } catch (err) {

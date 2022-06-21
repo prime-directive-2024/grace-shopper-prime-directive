f6ac84f799ccfcb62ca5eb830d5a30c34188c342
Tylen Carroll <tylenwcarroll@gmail.com>
2022-06-21 12:28:29 -0400

    added a loading screen to cart checkout

--- client/components/buttons/CheckOutButton.js
+++ client/components/buttons/CheckOutButton.js
@@ -1,6 +1,6 @@
 /** @format */
 
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { checkoutCart } from '../../store/cart';
 import { connect } from 'react-redux';
@@ -11,6 +11,7 @@ import axios from 'axios';
 // toast.configure();
 
 const CheckOutButton = (props) => {
+  const [isLoading, setIsLoading] = useState(false);
   const dispatch = useDispatch();
   console.log('PROPERTIES:', props);
   const product = {
@@ -19,6 +20,7 @@ const CheckOutButton = (props) => {
   };
   console.log('TOTAl in pennies:', product.price);
   async function handleToken(token) {
+    setIsLoading(true);
     // console.log({ token, addresses });
     const response = await axios.post(
       'https://vdw2id.sse.codesandbox.io/checkout',
@@ -29,23 +31,35 @@ const CheckOutButton = (props) => {
       // toast('Success! Check email for more details', { type: 'success' });
       dispatch(checkoutCart(props.auth.id, props.basket));
       alert('Thank you for your purchase!');
+      setIsLoading();
     } else {
       // toast('Somethign went wrong'), { type: 'error' };
       alert('something went wrong');
+      setIsLoading(false);
     }
   }
 
   // const handleClick = () => {};
   return (
-    <StripeCheckout
-      stripeKey="pk_test_51LCD7cIP1PyDvJD7Q5f5q9yvGzqFcBzoWJX4Zmsi63rMQvhYxZrYqYSyppHOcsOSoRfpP5zGb8c68HDRBeSg8WzP004HdLEQkR"
-      token={handleToken}
-      billingAddress
-      shippingAddress
-      amount={props.total * 100}
-      className="checkoutButton"
-      // onClick={() => handleClick()}
-    />
+    <div>
+      {isLoading ? (
+        <img
+          className="loading"
+          src="https://wallpaperaccess.com/full/2439067.gif"
+        />
+      ) : (
+        <></>
+      )}
+      <StripeCheckout
+        stripeKey="pk_test_51LCD7cIP1PyDvJD7Q5f5q9yvGzqFcBzoWJX4Zmsi63rMQvhYxZrYqYSyppHOcsOSoRfpP5zGb8c68HDRBeSg8WzP004HdLEQkR"
+        token={handleToken}
+        billingAddress
+        shippingAddress
+        amount={props.total * 100}
+        className="checkoutButton"
+        // onClick={() => handleClick()}
+      />
+    </div>
   );
 };
 
